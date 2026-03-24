d2cf936c50adaca51253ce60d2e5c00c6522df97
hasnainasif0761 <hasnainasif07610761@gmail.com>
2026-03-24 22:01:15 +0500

    9-Updation

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const ham = document.querySelector('.hamburger');
-const navlist = document.querySelector('.navlist');
+const ham = document.getElementById('hamburger');
+const navlist = document.querySelector('.navList');
 
 let Hamflag = false;
 
 ham.addEventListener('click',()=>{
     navlist.classList.toggle('openHam');
     if(Hamflag == false){
-        ham.innerHTML = `<i class="ri-close-line"></i>`
+        ham.innerHTML = `<i class="ri-close-line  text-3xl text-white"></i>`
         Hamflag = true
     }else
     {
-        ham.innerHTML = `<i class="ri-menu-fill class='text-3xl'"></i>`
+        ham.innerHTML = `<i class="ri-menu-fill  text-3xl text-white"></i>`
         Hamflag = false
     }
 })

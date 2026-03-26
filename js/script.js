87c4f30a9398681f9912fa054a6e9da24a052247
hasnainasif0761 <hasnainasif07610761@gmail.com>
2026-03-26 13:27:55 +0500

    19-Updation

--- js/script.js
+++ js/script.js
@@ -14,3 +14,49 @@ ham.addEventListener('click',()=>{
         Hamflag = false
     }
 })
+
+// ------------------------- Website Sidebar Section --------------------------------
+const sidebar = document.querySelector('.sidebar');
+const bgIcon = document.querySelector('.bag-icon')
+const closebar = document.querySelector('.closebar');
+
+bgIcon.addEventListener('click',()=>{
+    sidebar.classList.toggle('active');
+})
+
+closebar.addEventListener('click',()=>{
+    sidebar.classList.remove('active');
+})
+
+// ------------------ Add to card Logic Section ---------------
+const addtocart = document.querySelectorAll('.bg')
+const convertArray = Array.from(addtocart);
+
+convertArray.forEach((btn)=>{
+    btn.addEventListener('click',(e)=>{
+        const product = btn.closest('.product-box');
+        addtocartFunc(product)
+    })
+})
+
+const addtocartFunc = (productBox) =>{
+    const productImgSrc = productBox.querySelector('img').src;
+    const productTitle = productBox.querySelector('.product-title').textContent
+    const productPrice = productBox.querySelector('.product-price').textContent
+
+    const cardItem = productBox.querySelector('.product-title');
+    for(let items of cardItem){
+        if(items.textContent == productTitle){
+            alert('This Items is Already in the cart');
+            return;
+        }
+    }
+
+}
+
+
+
+
+
+
+

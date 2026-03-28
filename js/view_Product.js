6964616bb65b0c831913009b66bbc1a97df67763
hasnainasif0761 <hasnainasif07610761@gmail.com>
2026-03-28 13:35:33 +0500

    25-Updation

--- js/view_Product.js
+++ js/view_Product.js
@@ -21,22 +21,95 @@ onValue(productRef, (snapshot) => {
         const data = childSnapshot.val();
 
         container.innerHTML += `
-            <div class="bg-white shadow-lg rounded-xl p-3 hover:scale-105 transition">
+            <div class="bg-white shadow-lg rounded-xl p-3 hover:scale-105 transition flex flex-col">
                 <img src="${data.productImage}" 
-                     class="w-full h-40 object-cover rounded-lg">
+                     class="productImg w-full h-40 object-cover rounded-lg">
 
-                <h2 class="text-lg font-bold mt-2">
+                <h2 class="productName text-lg font-bold mt-2">
                     ${data.productName}
                 </h2>
 
-                <p class="text-sm text-gray-600">
+                <p class="productDesc text-sm text-gray-600 flex-grow">
                     ${data.productDsc}
                 </p>
 
-                <p class="text-green-600 font-bold mt-2">
+                <p class="productPrice text-green-600 font-bold mt-2">
                     Rs ${data.productPrice}
                 </p>
+                <div class="w-full flex justify-between mt-auto pt-2">
+                    <button class="editBtn w-[40%] active:scale-95 rounded-[5px] bg-green-500 text-white py-2 px-2 border text-[13px]">Edit Product</button>
+                    <button class="deletBtn w-[55%] active:scale-95 rounded-[5px] bg-red-500 text-white py-2  border text-[13px]">Delete Product</button>
+                </div>
             </div>
         `;
     });
+    let modal = document.getElementById('editModal');
+    let closeModal = document.getElementById('closeModal')
+    container.addEventListener('click',(e)=>{
+        if(e.target.classList.contains('editBtn')){
+            const card = e.target.closest('.bg-white');
+            editBtnFunc(card)
+            modal.classList.remove('hidden');
+            closeModal.addEventListener('click',()=>{
+                modal.classList.add('hidden');
+            })
+        }
+    })
+    container.addEventListener('click',(e)=>{
+        if(e.target.classList.contains('deleteBtn')){
+            deleteFunc(e.target.closest('.bg-white'))
+        }
+    })
 });
+
+let EditInputCont = document.querySelector('.EditInputCont');
+function editBtnFunc(cont){
+    const editImg = EditInputCont.querySelector('.img');
+    const editName = EditInputCont.querySelector('.name');
+    const editDesc = EditInputCont.querySelector('.desc');
+    const editPrice = EditInputCont.querySelector('.price');
+    const editBtn = EditInputCont.querySelector('.editnowBtn');
+
+    // UI Elements
+    const getUiImg = cont.querySelector('.productImg');
+    const getUiName = cont.querySelector('.productName');
+    const getUiDsc = cont.querySelector('.productDesc');
+    const getUiPrice = cont.querySelector('.productPrice');
+
+    // Values
+    editImg.value = getUiImg.src;
+    editName.value = getUiName.innerText;
+    editDesc.value = getUiDsc.innerText;
+    editPrice.value = getUiPrice.innerText;
+
+    editBtn.onclick = () => {
+
+
+        getUiImg.src = editImg.value;
+        getUiName.innerText = editName.value;
+        getUiDsc.innerText = editDesc.value;
+        getUiPrice.innerText = editPrice.value;
+
+        editImg.value = '';
+        editName.value = '';
+        editDesc.value = '';
+        editPrice.value = '';
+        let modal = document.getElementById('editModal');
+        modal.classList.add('hidden')
+        alert('Your Product is Updated');
+    }
+}
+
+
+// --------------------- I am Created Delete Function Code -------------------------------- //
+
+
+
+
+
+
+
+
+
+
+
